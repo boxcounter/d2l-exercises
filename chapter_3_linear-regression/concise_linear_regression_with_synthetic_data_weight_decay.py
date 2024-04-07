38a9298c2e86eac6f7a642af220ed3c270c7d1ee
Alex Lee <ns.boxcounter@gmail.com>
2024-04-07 11:46:29 +0800

    feat(ch3): Add weight decayed linear regression implementation.

--- chapter_3_linear-regression/concise_linear_regression_with_synthetic_data_weight_decay.py
+++ chapter_3_linear-regression/concise_linear_regression_with_synthetic_data_weight_decay.py
@@ -57,18 +57,22 @@ class SyntheticLinearDataLoader(DataLoader):
             yield X[start:stop], y[start:stop]
 
 
-class ConciseLinearRegressionModel(torch.nn.Module):
+class WeightDecayedLinearRegressionModel(torch.nn.Module):
     def __init__(
         self,
         features_dim: int,
         learning_rate: float,
+        weight_decay: float = 0.01,
     ) -> None:
         super().__init__()
         self._net = torch.nn.Linear(features_dim, 1)
         self._net.weight.data.normal_(mean = 0, std=random.random())
         self._net.bias.data.fill_(1)
-        self._optimizer = torch.optim.SGD(self._net.parameters(),
-                                         lr=learning_rate)
+        self._optimizer = torch.optim.SGD(
+            params=[self._net.weight, self._net.bias],
+            lr=learning_rate,
+            weight_decay=weight_decay,
+        )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         return self._net.forward(X)
@@ -86,32 +90,48 @@ class ConciseLinearRegressionModel(torch.nn.Module):
     def bias(self) -> float:
         return self._net.bias.data.detach().item()
 
+    @property
+    def l2_norm(self) -> float:
+        return torch.norm(self._net.weight.data).item()
+
 
 class Trainer:
     def __init__(
         self,
         data_loader: DataLoader,
-        model: ConciseLinearRegressionModel,
+        model: WeightDecayedLinearRegressionModel,
     ) -> None:
         self._dataloader = data_loader
         self._model = model
         self._loss_measurer = torch.nn.MSELoss()
+        self._losses: list[float] = []
 
     def train(
         self,
     ) -> None:
+        num_batches = 0
+        loss = torch.tensor(0.0)
         for X, y in self._dataloader.get_batch(True):
             self._model.train()
             y_pred = self._model(X)
             loss = self._loss_measurer(y_pred, y)
             self._model.backward(loss)
 
+            loss += self._loss_measurer(y_pred, y)
+            num_batches += 1
+
+        mean_loss = loss.item() / num_batches
+        self._losses.append(mean_loss)
+
+    @property
+    def losses(self) -> list[float]:
+        return self._losses
 
 class Evaluator:
     def __init__(
         self,
         data_loader: DataLoader,
-        model: ConciseLinearRegressionModel,
+        model: WeightDecayedLinearRegressionModel,
     ) -> None:
         self._dataloader = data_loader
         self._model = model
@@ -132,25 +152,38 @@ class Evaluator:
             self._losses.append(mean_loss)
             return mean_loss
 
-    def plot(
-        self,
-    ) -> None:
-        epoch = list(range(1, len(self._losses)+1))
-        losses = self._losses
+    @property
+    def losses(self) -> list[float]:
+        return self._losses
 
-        plt.xlabel('epoch')
-        plt.ylabel('loss')
-        plt.plot(epoch, losses)
-        plt.show()
+
+def plot_losses(
+    trainer: Trainer,
+    evaluator: Evaluator,
+    weight_decay: float,
+) -> None:
+    assert len(trainer.losses) == len(evaluator.losses), \
+        "Trainer and Evaluator losses length mismatch"
+    epochs = list(range(1, len(trainer.losses)+1))
+
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.plot(epochs, trainer.losses, 'b', label='train_loss')
+    plt.plot(epochs, evaluator.losses, 'r', label='valid_loss')
+    plt.legend()
+    plt.title(f"weight decay = {weight_decay}")
+    plt.show()
 
 
 def main():
     w = [13.9, 12.1]
     b = -1.9
-    epochs = 20
+    epochs = 30
+    weight_decay = 0.2
 
     data_loader = SyntheticLinearDataLoader(w, b)
-    model = ConciseLinearRegressionModel(features_dim=len(w), learning_rate=0.01)
+    model = WeightDecayedLinearRegressionModel(
+        features_dim=len(w), learning_rate=0.01, weight_decay=weight_decay)
     trainer = Trainer(data_loader, model)
     evaluator = Evaluator(data_loader, model)
 
@@ -161,8 +194,10 @@ def main():
 
     logger.info("Before training: w = {}, b = {}", w, b)
     logger.info("After  training: w = {}, b = {}", model.weights, model.bias)
+    logger.info("L2 norm of weights: {}", model.l2_norm)
 
-    evaluator.plot()
+    plot_losses(trainer, evaluator, weight_decay)
+
     logger.info("Done!")
 
 
